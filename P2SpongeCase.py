84f96ca70c6b11727a9ccc8fbd1d80233610f0c4
Brown,Branden <Branden.Brown2@cerner.com>
2018-11-15 08:32:32 -0600

    Sponge case woo

---
+++ P2SpongeCase.py
@@ -0,0 +1,56 @@
+from subprocess import check_call
+from sys import platform, argv
+from random import randint
+from os import path
+
+def copy_to_clip(string):
+    if(platform == 'darwin'):
+        cmd='echo \"' + string.strip()+'\" | pbcopy'
+    else:
+        cmd='echo \"' + string.strip()+'\" | clip'
+    return check_call(cmd, shell=True)
+
+def convert_to_spongecase(text):
+    userInput = text.lower()
+    spongeString = ""
+    for i in range(0,len(userInput)):
+        if not userInput[i].isalpha():
+            spongeString = spongeString + userInput[i]
+            continue
+        if(randint(0,100) > 50):
+            spongeString = spongeString + userInput[i].upper()
+        else:
+            spongeString = spongeString + userInput[i]
+    return spongeString
+
+firstLoop = False
+while(1):
+    userInput = ""
+    if(len(argv) == 2 and not firstLoop):
+        userInput = argv[1]
+        firstLoop = True
+    else:
+        userInput = raw_input('Enter the string or file name to switch to sponge case. Ctrl + C or "!exit!" to quit program.\n')
+    spongeString = ""
+    if(userInput == '!exit!'):
+        exit()
+    if(path.isfile(userInput)):
+        inputFile = open(userInput, "r")
+        spongeString = convert_to_spongecase(inputFile.read())
+        inputFile.close()
+        newFile = open(path.splitext(userInput)[0] + '_spongeCase.txt', "w+")
+        newFile.write(spongeString)
+        newFile.close()        
+    else:
+        spongeString = convert_to_spongecase(userInput)
+
+    
+    print spongeString
+    try:
+        copy_to_clip(spongeString)
+        print "*Sponge case (probably) copied to clipboard.*"
+    except:
+        continue
+        #do nothing
+
+    
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SpongeCase
 Python script to turn any alpha text into sponge case
+
+P2SpongeCase.py will work for Python 2
+
+P3SpongeCase.py will work for Python 3
+
+You can input a file as a command line arguments or at the prompt.
